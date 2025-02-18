b480a92478812ece94d8ba777a34cf6cc5b93040
abdelaleemadel <ahmedadel2912@gmail.com>
2025-02-18 15:30:34 +0200

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./Pages/HomePage";
+import BookingPage from "./Pages/BookingPage";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Pages/BookingPage.jsx
@@ -0,0 +1,5 @@
+function BookingPage() {
+  return <div></div>;
+}
+
+export default BookingPage;
